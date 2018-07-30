eb8c6058e1d28fbbd6d37c7c1dcf6a57e43c2f3e
carlos <carlos.molina.munoz@gmail.com>
2018-07-29 22:29:56 -0400

    por fin pude obtener los usuarios relacionados a una tarea

--- app/controllers/tareas_controller.rb
+++ app/controllers/tareas_controller.rb
@@ -6,9 +6,16 @@ class TareasController < ApplicationController
 
   def show
     # @tareadetalle = Tarea.find_by name: :tarea_id
-    @idedelatarea = params[:tarea]
-    @usertask = UserTask.where(tarea: :tarea_id)
-    @usuariostarea = User.where(id: @usertask)
+    @tarea = Tarea.find(params[:id])
+    # @ut = UserTask.where(tarea_id: @tarea.id)
+    @usuariostarea = User.where(id: UserTask.where(tarea_id: @tarea.id))
+
+
+
+
+    # @idedelatarea = params[:tarea]
+    # @usertask = UserTask.where(tarea: :tarea_id)
+    # @usuariostarea = User.where(id: @usertask)
     # @usrcompletada = User.where(user: @usertask.user_id)
     # @tareascompletadas = UserTask.where(user: current_user).count
     # @tareastotales = Tarea.all.count
